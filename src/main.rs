5702eb438cdf71c4ac0f48362c32e56bb7426acf
simon-an <26556185+simon-an@users.noreply.github.com>
2021-07-28 09:08:32 +0200

    F10 Stop. F8 Adern.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ mod screen;
 fn main() {
 
     let running: Arc<Mutex<bool>>= Arc::new(Mutex::new(true));
+    let counter: Arc<Mutex<u32>>= Arc::new(Mutex::new(0));
 
     // Rapidfire for videogames.
     let x = Arc::clone(&running);
@@ -17,49 +18,83 @@ fn main() {
             sleep(Duration::from_millis(50));
             RKey.release();
             sleep(Duration::from_millis(2100));
+            
         }
     });
 
-    let y = Arc::clone(&running);
-    F6Key.bind(move || {
-        println!("i have enought water");
-        *y.lock().unwrap() = false;
-    });
-
     let r3 = Arc::clone(&running);
-    F7Key.bind(move || {
+    let c1 = Arc::clone(&counter);
+    F6Key.bind(move || {
         *r3.lock().unwrap() = true;
-        while *r3.lock().unwrap() {
-            println!("Give me my wood");
+        *c1.lock().unwrap() = 0;
+        FKey.press();
+        sleep(Duration::from_millis(50));
+        FKey.release();
+        while *r3.lock().unwrap() && *c1.lock().unwrap() < 50 {
+            println!("Give me my wood {}", *c1.lock().unwrap());
             MiddleButton.press();
             sleep(Duration::from_millis(450));
             MiddleButton.release();
             sleep(Duration::from_millis(700));
+            *c1.lock().unwrap() += 1;
         }
-    });
-
-    let r4 = Arc::clone(&running);
-    F8Key.bind(move || {
-        println!("i have enought wood");
-        *r4.lock().unwrap() = false;
+        FKey.press();
+        sleep(Duration::from_millis(50));
+        FKey.release();
     });
 
     let r5 = Arc::clone(&running);
-    F9Key.bind(move || {
+    let c2 = Arc::clone(&counter);
+    F7Key.bind(move || {
+        *c2.lock().unwrap() = 0;
         *r5.lock().unwrap() = true;
-        while *r5.lock().unwrap() {
-            println!("Give me my iron");
+        FKey.press();
+        sleep(Duration::from_millis(50));
+        FKey.release();
+        while *r5.lock().unwrap()  && *c2.lock().unwrap() < 18 {
+            println!("Give me my iron {}", *c2.lock().unwrap());
             MiddleButton.press();
-            sleep(Duration::from_millis(450));
+            sleep(Duration::from_millis(420));
             MiddleButton.release();
             sleep(Duration::from_millis(900));
+            *c2.lock().unwrap() += 1;
         }
+        FKey.press();
+        sleep(Duration::from_millis(50));
+        FKey.release();
+    });
+
+    let r7 = Arc::clone(&running);
+    let c4 = Arc::clone(&counter);
+    F8Key.bind(move || {
+        *c4.lock().unwrap() = 0;
+        *r7.lock().unwrap() = true;
+        while *r7.lock().unwrap()  && *c4.lock().unwrap() < 18 {
+            FKey.press();
+            sleep(Duration::from_millis(50));
+            FKey.release();
+            while *c4.lock().unwrap() < 18 {
+                println!("Give me my iron {}", *c4.lock().unwrap());
+                MiddleButton.press();
+                sleep(Duration::from_millis(420));
+                MiddleButton.release();
+                sleep(Duration::from_millis(900));
+                *c4.lock().unwrap() += 1;
+            }
+            *c4.lock().unwrap() = 0;
+            FKey.press();
+            sleep(Duration::from_millis(50));
+            FKey.release();
+            sleep(Duration::from_millis(8000));
+         }
     });
 
     let r6 = Arc::clone(&running);
+    let c3 = Arc::clone(&counter);
     F10Key.bind(move || {
         println!("i have enought iron");
         *r6.lock().unwrap() = false;
+        *c3.lock().unwrap() = 0;
     });
 
 
